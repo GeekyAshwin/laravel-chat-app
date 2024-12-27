b44e88635d677e45ca7afafd15c0d0384e2a8ea7
Ashwiner <techmindashwin78@gmail.com>
2024-12-27 22:04:06 +0530

    added pusher

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Message;
+use Illuminate\Http\Request;
+use Throwable;
+use App\Events\MessageSent;
+
+class MessageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        try {
+            $message = Message::create([
+                'sent_to' => 2,
+                'sent_by' => 1,
+                'message' => $request->input('message'),
+                'has_attachment' => false
+            ]);
+            event(new MessageSent($message));
+
+            return response()->json([
+                'message' => 'Message sent',
+                'data' => $message
+            ]);
+
+        } catch (Throwable $exception) {
+            dd($exception);
+
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
